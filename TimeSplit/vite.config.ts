c1d224f80412bbb3b2b74750f50730c630517682
dimoita <di.moita@gmail.com>
2026-01-23 12:06:26 -0300

    Update vite.config.ts

--- TimeSplit/vite.config.ts
+++ TimeSplit/vite.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'esnext',
     outDir: 'dist',
-    sourcemap: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
   },
   server: {
     port: 3000,
+    host: true,
   },
 });
