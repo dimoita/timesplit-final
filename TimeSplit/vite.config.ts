b76cdeb70a7af5455ac4295bcb4f4c1a6ac0436f
dimoita <di.moita@gmail.com>
2026-01-24 10:58:50 -0300

    Update vite.config.ts

--- TimeSplit/vite.config.ts
+++ TimeSplit/vite.config.ts
@@ -4,22 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
     outDir: 'dist',
-    // ESTA É A CORREÇÃO DO ERRO 'UE':
-    modulePreload: {
-      polyfill: false,
-    },
-    // Garante que o build não tente minificar nomes de variáveis críticas agora
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
+  }
 });
